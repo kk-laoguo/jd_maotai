fd658d960a4aaf1fab68a79436e3c307ebecfd9c
fengjiannan2010 <fengjiannan2010@163.com>
2020-12-08 17:34:27 +0800

    release

---
+++ clear.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -fr cookies
+rm -f QRcode.png
+rm -f jdBuyMask.log
+rm -fr __pycache__
+
+#git commit . -m "release"
+#git push -u origin main
+#fengjiannan2010@163.com
+#hit18626195610@
